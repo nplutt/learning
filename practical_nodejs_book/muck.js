4cba362c3a9c292ab43f6a2d88fc02e1bae630c6
Nick Plutt <nplutt@gmail.com>
2017-09-25 20:47:19 -0500

    working my way through chapter 1

---
+++ practical_nodejs_book/muck.js
@@ -0,0 +1,14 @@
+
+var obj = {
+  color: 'green',
+  type: 'suv'
+};
+
+console.log(obj.color);
+
+var obj = function() {
+  this.color = 'green',
+  this.type = 'suv'
+};
+
+//console.log(obj().color);
