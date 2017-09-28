321014fabd9dcf99a14cce8fe3726b3ba1356533
Nick Plutt <nplutt@gmail.com>
2017-09-28 18:40:50 -0500

    fixing some session issues

--- practical_nodejs_book/chapter_2/personal_blog_poject/svc/app.js
+++ practical_nodejs_book/chapter_2/personal_blog_poject/svc/app.js
@@ -3,15 +3,16 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logHandler = require('./handlers/errorLogging');
 const errorHandler = require('./handlers/errorHandling');
-const sessionHandler = require('./handlers/sessionHandler');
+const authHandler = require('./handlers/sessionHandler');
 const index = require('./routes/index');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sessionHandler);
+app.use(authHandler());
 
 app.use('/', index);
 
--- practical_nodejs_book/chapter_2/personal_blog_poject/svc/handlers/sessionHandler.js
+++ practical_nodejs_book/chapter_2/personal_blog_poject/svc/handlers/sessionHandler.js
@@ -2,15 +2,14 @@ const session = require('express-session');
 const mongoStore = require('connect-mongo')(session);
 const connection = require('../db/db');
 
-function authSession(err, req, res, next) {
-  session({
+function authSession() {
+  return session({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: false, // don't create session until something stored
     resave: false, //don't save session if unmodified
     store: new mongoStore({ mongooseConnection: connection }),
     cookie: { secure: process.env.ENVIRONMENT === 'production' }
   });
-  next();
 }
 
 module.exports = authSession;
